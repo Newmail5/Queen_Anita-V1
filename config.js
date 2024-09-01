1f6b772a4f7983c2e3656e83b440a78b5ae33f68
Newmail5 <new39103@gmail.com>
2024-09-01 17:35:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,15 +4,15 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '2349039996138'
+global.nomerowner = ["2349039996138"]
 
 // Apikey 
 
 global.skizo = 'David Cyril'
 global.casterix = 'DeeCeeXxx'
 //watermark 
-global.packname = '*DAVID CYRIL*'
+global.packname = '*MFCLONE*'
 global.author = 'QUEEN_ANITA-V1'
 
 // cpanel 
